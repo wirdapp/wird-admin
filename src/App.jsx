85179635da1e58f4689a47a5f2a4b0d37f665e88
tamimibus <tamimi@Tamimis-MacBook-Pro-2.local>
2024-01-30 08:09:44 +0300

    feat:add notification

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
-
+import { NotificationContainer } from "react-notifications";
+import 'react-notifications/lib/notifications.css';
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
+      <NotificationContainer />
       <RouterProvider router={router} />
     </I18nextProvider>
   );
